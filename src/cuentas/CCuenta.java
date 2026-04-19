d1b9c6110fcdaeffa65321b10eee8caa32b38cba
SeCaP91 <sergio.cambronero.ponce@gmail.com>
2026-04-19 12:57:02 +0200

    Segundo Commit (refactorizado)

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -1,40 +1,35 @@
+
+package cuentas;
+
 public class CCuenta {
-
-
+    // Atributos de la clase:
     private String nombre;
     private String cuenta;
     private double saldo;
-    private double tipoInterés;
+    private double tipoInteres;
 
-    public CCuenta()
+    public CCuenta() { }
+
+    public CCuenta(String nombre, String cuenta, double saldo, double tipoInteres)
     {
+        this.nombre = nombre;
+        this.cuenta = cuenta;
+        this.saldo = saldo;
+        this.tipoInteres = tipoInteres;
     }
 
-    public CCuenta(String nom, String cue, double sal, double tipo)
-    {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
-    }
-
-    public double estado()
-    {
-        return saldo;
-    }
+    public double estado() { return this.saldo; }
 
     public void ingresar(double cantidad) throws Exception
     {
-        if (cantidad<0)
-            throw new Exception("No se puede ingresar una cantidad negativa");
-        saldo = saldo + cantidad;
+        if ( cantidad < 0 ) { throw new Exception("No se puede ingresar una cantidad negativa"); }
+        this.saldo = this.saldo + cantidad;
     }
 
     public void retirar(double cantidad) throws Exception
     {
-        if (cantidad <= 0)
-            throw new Exception ("No se puede retirar una cantidad negativa");
-        if (estado()< cantidad)
-            throw new Exception ("No se hay suficiente saldo");
-        saldo = saldo - cantidad;
+        if ( cantidad <= 0 ) { throw new Exception ("No se puede retirar una cantidad nula o negativa"); }
+        if ( estado() < cantidad ) { throw new Exception ("No se hay suficiente saldo"); }
+        this.saldo = this.saldo - cantidad;
     }
 }
--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -1,23 +1,32 @@
+package cuentas;
+
 public class Main {
 
     public static void main(String[] args) {
-        CCuenta miCuenta;
+        CCuenta cuenta1;
         double saldoActual;
+        float cantidad1, cantidad2;
+        
+        // Dando valor a las variables:
+        cantidad1 = 2300;
+        cantidad2 = 695;
+        cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
+        
+        // Mostrando el saldo de la cuenta:
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es " + saldoActual);
 
-        miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = miCuenta.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        // Realizando la operativa:
+        operativa_cuenta(cuenta1,cantidad1, cantidad2);
+    }
 
+    private static void operativa_cuenta(CCuenta cuenta1, float cantidad1, float cantidad2) {
         try {
-            miCuenta.retirar(2300);
-        } catch (Exception e) {
-            System.out.print("Fallo al retirar");
-        }
+            cuenta1.retirar(cantidad1);
+        } catch (Exception e) { System.out.print("Fallo al retirar"); }
         try {
             System.out.println("Ingreso en cuenta");
-            miCuenta.ingresar(695);
-        } catch (Exception e) {
-            System.out.print("Fallo al ingresar");
-        }
+            cuenta1.ingresar(cantidad2);
+        } catch (Exception e) { System.out.print("Fallo al ingresar"); }
     }
 }
